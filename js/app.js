ad1e483b590e9f29a9c89cfd8dfb59b38032db17
Guilherme Estrela <guilherme.estrela@clickbus.com>
2019-02-11 09:37:19 -0200

    Init Player and render

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 83;
 };
 
 // Update the enemy's position, required method for game
@@ -21,14 +23,51 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/**
+ * Class: Player
+ *
+ * @constructor
+ */
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 202;
+    this.y = 332;
+};
+
+/**
+ * Update Player position
+ *
+ * @param dt
+ */
+Player.prototype.update = function(dt) {
+
+};
+
+/**
+ * Render Player method
+ */
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+/**
+ * Handle input to change Player position
+ *
+ * @param key
+ */
+Player.prototype.handleInput = function(key) {
+    console.log('Key', key);
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+var enemy = new Enemy();
+allEnemies.push(enemy);
+
+var player = new Player();
 
 
 
